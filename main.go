dd86cee19a93efcbd637149ccd2416f6dd0f7074
Pinkyuu <maksim7.25@mail.ru>
2023-09-29 01:39:02 +0700

    Fix problem in rem

--- main.go
+++ main.go
@@ -54,23 +54,24 @@ func (a Insects) PrintElement() {
 }
 
 func Sound(a Creature) {
-	a.PrintElement()
+	a.PrintElement() // Вывод интерфейса
 }
 
-func splitValues(values string) []string {
-	re := regexp.MustCompile(`\s*,\s*`)
+func splitValues(values string) []string { // разбиваем строку (*1*.*2*.*3*) на блоки block[0] = *1*, block[1] = *2*, block[2] = *3*
+	re := regexp.MustCompile(`\s*,\s*`) // Используем регулярное выражение
 	blocks := re.Split(values, -1)
 	return blocks
 }
 
+// Добавление рыбы
 func AddFish(container *list.List, matches []string, line string) {
 	if len(matches) > 1 {
-		values := matches[1]
+		values := matches[1]          // параметры переданные в функция для структуры Fish
 		blocks := splitValues(values) // Разбиваем значения на блоки
-		if len(blocks) == 2 {
-			var a Fish // структура типа Fish
-			a.name = blocks[0]
-			a.areal = blocks[1]
+		if len(blocks) == 2 {         // 1 блок - имя, 2-ой блок - ареал
+			var a Fish          // структура типа Fish
+			a.name = blocks[0]  // Записываем 1-ый блок в имя
+			a.areal = blocks[1] // Записываем 2-ой блок в ариал
 			container.PushBack(a)
 		} else {
 			fmt.Println("Not enough parameters for", line) // Если внутри скобок не заданы параметры
@@ -78,49 +79,51 @@ func AddFish(container *list.List, matches []string, line string) {
 	}
 }
 
+// Добавление птицы
 func AddBird(container *list.List, matches []string, line string) (err bool) {
 	if len(matches) > 1 {
-		values := matches[1]
+		values := matches[1]          // параметры переданные в функция для структуры Bird
 		blocks := splitValues(values) // Разбиваем значения на блоки
-		if len(blocks) == 2 {
-			var a Bird // структура типа Bird
-			a.name = blocks[0]
-			if StrToFloat, err := strconv.ParseFloat(blocks[1], 64); err == nil {
-				a.speed = StrToFloat
+		if len(blocks) == 2 {         // 1 блок - имя, 2-ой блок - скорость
+			var a Bird                                                            // структура типа Bird
+			a.name = blocks[0]                                                    // Записывается имя из блока
+			if StrToFloat, err := strconv.ParseFloat(blocks[1], 64); err == nil { // Конвертация string в float64
+				a.speed = StrToFloat // Записываем 2-ой блок в скорость
 			} else {
 				panic(err) // если string не конвертировался в float64
 			}
-			container.PushBack(a)
-			return true
+			container.PushBack(a) // Добавление в контейнер
+			return true           // Успешно добавился
 		} else {
 			fmt.Println("Not enough parameters for", line) // Если внутри скобок не заданы параметры
-			return false
+			return false                                   // Ошибка
 		}
 	} else {
-		fmt.Println("No parameters found", line)
+		fmt.Println("No parameters found", line) // Не найден параметр
 		return false
 	}
 }
 
+// Добавление насекомого
 func AddInsects(container *list.List, matches []string, line string) (err bool) {
 	if len(matches) > 1 {
-		values := matches[1]
+		values := matches[1]          // параметры переданные в функция для структуры Insects
 		blocks := splitValues(values) // Разбиваем значения на блоки
-		if len(blocks) == 3 {
-			var a Insects // структура типа Insects
-			a.name = blocks[0]
-			if StrToFloat, err := strconv.ParseFloat(blocks[1], 64); err == nil {
-				a.size = StrToFloat
+		if len(blocks) == 3 {         // 1 блок - имя, 2-ой блок - размер, 3 блок - дата обнаружения
+			var a Insects                                                         // структура типа Insects
+			a.name = blocks[0]                                                    // // Записываем 1-ый блок в имя
+			if StrToFloat, err := strconv.ParseFloat(blocks[1], 64); err == nil { // Конвертация string в float64
+				a.size = StrToFloat // Записываем 2-ой блок в размеры
 			} else {
 				panic(err) // если string не конвертировался в float64
 			}
-			if checkdate(blocks[2]) {
-				a.date = blocks[2]
+			if checkdate(blocks[2]) { // Проверка даты на валидность
+				a.date = blocks[2] // Проверка выдала true
 			} else {
-				fmt.Println("Invalid date")
+				fmt.Println("Invalid date") // Проверка выдала false
 				return false
 			}
-			container.PushBack(a)
+			container.PushBack(a) // Добавление в контейнер
 			return true
 		} else {
 			fmt.Println("Not enough parameters for", line) // Если внутри скобок не заданы параметры
@@ -136,11 +139,11 @@ func AddInsects(container *list.List, matches []string, line string) (err bool) 
 func add(line string, container *list.List) {
 	re := regexp.MustCompile(`\(([^)]+)\)`) // Регулярное выражение
 	matches := re.FindStringSubmatch(line)
-	if strings.Contains(line, "Fish") {
+	if strings.Contains(line, "Fish") { // В строке есть слово "Fish"
 		AddFish(container, matches, line)
-	} else if strings.Contains(line, "Bird") {
+	} else if strings.Contains(line, "Bird") { // В строке есть слово "Bird"
 		AddBird(container, matches, line)
-	} else if strings.Contains(line, "Insects") {
+	} else if strings.Contains(line, "Insects") { // В строке есть слово "Insects"
 		AddInsects(container, matches, line)
 	}
 }
@@ -150,28 +153,26 @@ func rem(container *list.List, line string) {
 	words := strings.SplitN(line, " ", 2) // разбивает строку на подстроки, используя в качестве разделителя пробел (" "). Второй аргумент, 2, указывает на то, что она должна разделить строку не более чем на две подстроки.
 	if len(words) > 1 {
 		result := words[1]
-		fmt.Println(result)
-		for e := container.Front(); e != nil; e = e.Next() {
-			if fish, ok := e.Value.(Fish); ok {
+		for e := container.Front(); e != nil; e = e.Next() { // Удаление по типу структуры
+			if fish, ok := e.Value.(Fish); ok { // Если рыба
 				if fish.name == result {
 					container.Remove(e)
-					fmt.Println("Удалил")
+					e = container.Front()
 				}
-			} else if bird, ok := e.Value.(Bird); ok {
+			} else if bird, ok := e.Value.(Bird); ok { // Если птица
 				if bird.name == result {
 					container.Remove(e)
-					fmt.Println("Удалил")
+					e = container.Front()
 				}
-			} else if insects, ok := e.Value.(Insects); ok {
+			} else if insects, ok := e.Value.(Insects); ok { // Если насекомое
 				if insects.name == result {
 					container.Remove(e)
-					fmt.Println("Удалил")
+					e = container.Front()
 				}
 			}
 		}
 	} else {
-		fmt.Println("Not found word")
-		return
+		fmt.Println("Not found word") // Если не найдено слово
 	}
 }
 
@@ -182,6 +183,7 @@ func print(container list.List) {
 			Sound(creature) // В зависимости от типа структуры Fish, Bird, Insects - будет "звук", которые выводит его параметры
 		}
 	}
+	fmt.Println("________________________")
 }
 
 const nvar int16 = 9 // Номер варианта
@@ -222,11 +224,11 @@ func main() {
 	}
 
 	for _, line := range lines {
-		if strings.HasPrefix(line, "ADD") {
+		if strings.HasPrefix(line, "ADD") { // Проверяет, первое слово в строке "ADD"
 			add(line, container)
-		} else if strings.HasPrefix(line, "REM") {
+		} else if strings.HasPrefix(line, "REM") { // Проверяет, первое слово в строке "REM"
 			rem(container, line)
-		} else if strings.HasPrefix(line, "PRINT") {
+		} else if strings.HasPrefix(line, "PRINT") { // Проверяет, первое слово в строке "PRINT"
 			print(*container)
 		}
 	}
